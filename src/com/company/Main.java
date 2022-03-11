aaa2332e58fe7fb05bd557e20361cda310a68e37
tomm8231 <tomm8231@stud.kea.dk>
2022-03-11 13:55:10 +0100

    Fuck af Lasse

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World");
-        System.out.println("Super program!");
         System.out.println("Enig!");
         System.out.println("100p");
     }
