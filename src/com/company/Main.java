7ddba5108dc50bed9812fa7dd9a0fa7ccae9cc2a
LasseDall <lass794rrstud.kea.dk>
2022-03-11 13:55:07 +0100

    enig slettet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello World");
         System.out.println("Super program!");
-        System.out.println("Enig!");
         System.out.println("100p");
     }
 }
