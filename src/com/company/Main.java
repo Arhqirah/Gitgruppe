a5d20188222bd302d217f33f491a21bb6e4d4d03
LasseDall <lass794rrstud.kea.dk>
2022-03-11 13:58:18 +0100

    Duper

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("Hello World");
         System.out.println("Super program!");
+        System.out.println("Super duper!");
         System.out.println("100p");
     }
 }
